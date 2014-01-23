5ca52b8d252fb70f420eb1080031287ff2665281
Max-Goad <max_goad@hotmail.com>
2014-01-23 14:11:48 -0500

    Adds basic Entity funcs.

--- Project1/Project1/Entity.cpp
+++ Project1/Project1/Entity.cpp
@@ -1,5 +1,17 @@
 #include "Entity.h"
 
 Entity::Entity(){
+	pos.x = 0;
+	pos.y = 0;
+}
 
+float Entity::getX() { return pos.x; }
+float Entity::getY() { return pos.y; }
+void Entity::setX(float x) { pos.x = x; }
+void Entity::setY(float y) { pos.y = y; }
+
+void Entity::setPos(float x, float y)
+{
+	pos.x = x;
+	pos.y = y;
 }
--- Project1/Project1/Entity.h
+++ Project1/Project1/Entity.h
@@ -1,9 +1,27 @@
 #ifndef ENTITY_H
 #define ENTITY_H
-// test comment
+
+#include <DirectXMath.h>
+#include <string>
+
+using namespace std;
+using namespace DirectX;
+
+
 class Entity {
-public:
-	Entity();
+	public:
+		Entity();
+
+	private:
+		XMFLOAT2 pos;
+
+	public:
+		float getX();
+		float getY();
+		void setX(float);
+		void setY(float);
+
+		void setPos(float, float);
 };
 
 #endif
