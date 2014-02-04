e18272c47f2072349f6a14d147d1ad42424fcec6
Max-Goad <max_goad@hotmail.com>
2014-02-04 09:37:41 -0500

    (DNC) Fixed XInput version for Windows 7. SEE WIKI FOR LINKING INSTRUCTIONS!!!

--- Project1/Project2/CXBOXController.h
+++ Project1/Project2/CXBOXController.h
@@ -11,7 +11,7 @@
 // Now, the XInput Library
 // NOTE: COMMENT THIS OUT IF YOU ARE NOT USING
 // A COMPILER THAT SUPPORTS THIS METHOD OF LINKING LIBRARIES
-#pragma comment(lib, "XInput.lib")
+//#pragma comment(lib, "XInput.lib")
 
 // XBOX Controller Class Definition
 class CXBOXController
