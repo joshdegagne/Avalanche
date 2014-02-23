3c03e2d2c1726a55100f4e959bb748350083e5b6
Cameron Davies <cwedavies@gmail.com>
2014-02-23 12:39:10 -0500

    Added type associations to ViewModel

--- Source/Engine/Engine/ViewModel.cpp
+++ Source/Engine/Engine/ViewModel.cpp
@@ -1,20 +1,32 @@
 
+#include "EntityType.h"
 #include "ViewModel.h"
 
+//CONSTRUCTOR
 template <class T>
-ViewModel<T>::ViewModel()
+ViewModel<T>::ViewModel(EntityType associatedType)
 {
 	entityList = new list<T>();
+
+	this->associatedType = associatedType;
 }
 
+//DESTRUCTOR
 template<class T>
 ViewModel<T>::~ViewModel()
 {
 	delete entityList;
 }
 
+
 template <class T>
 void ViewModel<T>::Add(const T& entity)
 {
 	entityList->push_front(entity);
+}
+
+template <class T>
+EntityType ViewModel<T>::GetAssociatedType()
+{
+	return associatedType;
 }
--- Source/Engine/Engine/ViewModel.h
+++ Source/Engine/Engine/ViewModel.h
@@ -1,21 +1,26 @@
 #pragma once
 
-#include "IViewModel.h"
+
 #include <list>
+#include "IViewModel.h"
 
 class Model;
+enum class EntityType;
 
 template <class T>
 class ViewModel : IViewModel
 {
 public:
-	ViewModel();
+	ViewModel(EntityType);
 	~ViewModel();
 
 	virtual bool InitializeVertexModels(ID3D11Device* d3dDevice);
 	virtual bool Render(ID3D11DeviceContext* deviceContext,  XMFLOAT4X4 viewMatrix, XMFLOAT4X4 projectionMatrix, ColorShader* colorShader, TextureShader* textureShader);
-	void Add(const T&);
+	
+	void		Add(const T&);
+	EntityType GetAssociatedType();
 
 private:
 	std::list<T>*	entityList;
+	EntityType		associatedEntity;
 };
