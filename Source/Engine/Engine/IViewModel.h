3819fb9fc038cad7ea0f7ce8ac2c4da80b0b9030
Cameron Davies <cwedavies@gmail.com>
2014-02-23 15:31:58 -0500

    GameModel now inherits from IViewModel

--- Source/Engine/Engine/IViewModel.h
+++ Source/Engine/Engine/IViewModel.h
@@ -1,14 +1,16 @@
 #pragma once
 
+#include "DirectXHelper.h"
+#include <d3d11.h>
+
+using namespace DirectX;
+
 class ColorShader;
-class ID3D11Device;
-class ID3D11DeviceContext;
 class TextureShader;
-struct XMFLOAT4X4;
 
 class IViewModel
 {
 public:
-	virtual bool InitializeVertexModels(ID3D11Device* d3dDevice);
-	virtual bool Render(ID3D11DeviceContext* deviceContext,  XMFLOAT4X4 viewMatrix, XMFLOAT4X4 projectionMatrix, ColorShader* colorShader, TextureShader* textureShader);
+	virtual bool InitializeVertexModels(ID3D11Device* d3dDevice) = 0;
+	virtual bool Render(ID3D11DeviceContext* deviceContext,  DirectX::XMFLOAT4X4 viewMatrix, DirectX::XMFLOAT4X4 projectionMatrix, ColorShader* colorShader, TextureShader* textureShader) = 0;
 };
