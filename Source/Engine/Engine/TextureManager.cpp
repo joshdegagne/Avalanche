d82c454dc3ece052f898a5406d26ed16b7bd71ae
huynhrichy <richardbaohuynh@gmail.com>
2014-03-02 19:46:25 -0500

    Starts fleshing out TextureManager functionality

--- Source/Engine/Engine/TextureManager.cpp
+++ Source/Engine/Engine/TextureManager.cpp
@@ -2,8 +2,9 @@
 #include "ITexture.h"
 #include "Texture.h"
 
-TextureManager::TextureManager()
+TextureManager::TextureManager(ID3D11Device* d)
 {
+	device = d;
 }
 
 bool TextureManager::initialize()
@@ -16,9 +17,19 @@ void TextureManager::update(float elapsedTime)
 
 }
 
-Texture* TextureManager::loadTexture(WCHAR* filename) 
+Texture* TextureManager::loadTexture(WCHAR* filename)
 {
-	return NULL;
+	// Attempting to work like Texture's Initialize method or ViewModel initializeTexture?
+
+	//Texture texture = new Texture();
+	Texture* texture = new Texture;
+
+	// Save or load into memory from file path?
+
+	//CreateDDSTextureFromMemory(device, NULL, NULL, NULL, texture->GetTexture(), NULL);
+	
+	//return &texture;
+	return texture;
 }
 
 SpriteTexture* TextureManager::loadSpriteTexture(WCHAR* filename, float width) 
@@ -26,6 +37,7 @@ SpriteTexture* TextureManager::loadSpriteTexture(WCHAR* filename, float width)
 	return NULL;
 }
 
+// Deletes pointer to ITexture
 void TextureManager::unloadTexture(ITexture* texture)
 {
 }
--- Source/Engine/Engine/TextureManager.h
+++ Source/Engine/Engine/TextureManager.h
@@ -10,19 +10,19 @@
 class TextureManager : public IManager
 {
 public:
-	TextureManager();
+	TextureManager(ID3D11Device*);
 
-	bool initialize();
-	void update(float);
+	bool initialize(); // What for, again?
+	void update(float);  // For SpriteTextures?
 
 	Texture* loadTexture(WCHAR*); // Create new Texture, save pointer, initialize Texture, return pointer to Texture... Ref to D3D device?
 	SpriteTexture* loadSpriteTexture(WCHAR*, float); 
-	void unloadTexture(ITexture*);
+	void unloadTexture(ITexture*); // Makes deletion
 
 	// initialize texture taking in reference to game?
 
 	//template<class T> void add(T* entity);
 private:
 	//ArrayList<ViewModelBase>* models;
-	//ID3D11Device
+	ID3D11Device* device;
 };
