299a4d141263e898a6b29191eed568b897096ab1
max-goad <maxlgoad@gmail.com>
2014-03-15 11:54:46 -0400

    Added rudimentary CollisionManager

--- Source/Engine/Engine/collisionmanager.cpp
+++ Source/Engine/Engine/collisionmanager.cpp
@@ -1,58 +1,50 @@
-#include "collisionmanager.h"
+#include "CollisionManager.h"
+#include "Player.h"
+#include "Obstacle.h"
+#include "BoundingBox.h"
 
-
-CollisionManager::CollisionManager(void)
+void CollisionManager::addPlayerReference(Player* p)
 {
+	players.add(p);
 }
 
-
-CollisionManager::~CollisionManager(void)
+void CollisionManager::addObstacleReference(Obstacle* o)
 {
+	obstacles.add(o);
 }
 
-void CollisionManager::MoveIfNotColliding(Robot* a, Robot* b, void (Robot::*FuncPtr)(void))
+void CollisionManager::checkForCollisions()
 {
-	// Make a copy of the robot position before it moves
-	XMFLOAT3 initialRobotPosition = a->GetPosition();
-
-	// Move the robot
-	(a->*FuncPtr)();
-
-	// If the move caused a collision, restore the robot to its initial position
-	if (AreColliding(a, b))	
+	for (int i = 0; i < players.size(); ++i)
 	{
-		a->SetPosition(initialRobotPosition);
+		for (int j = 0; j < obstacles.size(); ++j)
+		{
+			if (intersects(players.elementAt(i)->getBound(), obstacles.elementAt(j)->getBound()))
+			{
+				//players.elementAt(i)->collideWithObstacle(obstacles.elementAt(j));
+				return;
+			}
+		}
+
+		for (int j = 0; j < players.size(); ++j)
+		{
+			if (intersects(players.elementAt(i)->getBound(), players.elementAt(j)->getBound()))
+			{
+				//players.elementAt(i)->collideWithPlayer(players.elementAt(j));
+				return;
+			}
+		}
 	}
 }
 
-bool CollisionManager::AreColliding(Robot* a, Robot* b)
+
+bool CollisionManager::intersects(BoundingBox* boundA, BoundingBox* boundB)
 {
-	if (a->GetBoundaryType() == "circle")
-	{
-		float horizontal = a->GetPosition().x - b->GetPosition().x;
-		float vertical = a->GetPosition().y - b->GetPosition().y;
+	if (boundA == boundB)
+		return false;
 
-		float hypotenuse = sqrt((horizontal*horizontal) + (vertical*vertical));
-
-		return hypotenuse < a->GetHeight()*2 || hypotenuse < b->GetHeight()*2;
-	}
-	else if (a->GetBoundaryType() == "square")
-	{
-		float ATopEdge = a->GetPosition().y + a->GetHeight()/2;
-		float ABottomEdge = a->GetPosition().y - a->GetHeight()/2;
-		float ARightEdge = a->GetPosition().x + a->GetWidth()/2;
-		float ALeftEdge = a->GetPosition().x - a->GetWidth()/2;
-
-		float BTopEdge = b->GetPosition().y + b->GetHeight()/2;
-		float BBottomEdge = b->GetPosition().y - b->GetHeight()/2;
-		float BRightEdge = b->GetPosition().x + b->GetWidth()/2;
-		float BLeftEdge = b->GetPosition().x - b->GetWidth()/2;
-
-		return ALeftEdge < BRightEdge && 
-			ARightEdge > BLeftEdge && 
-			ATopEdge > BBottomEdge && 
-			ABottomEdge < BTopEdge;
-	}
-
-	return false;
+	XMFLOAT3 A_POS = *(boundA->getPosition());
+	XMFLOAT3 A_DIM = *(boundA->getDimensions());
+	XMFLOAT3 B_POS = *(boundB->getPosition());
+	XMFLOAT3 B_DIM = *(boundB->getDimensions());
 }
