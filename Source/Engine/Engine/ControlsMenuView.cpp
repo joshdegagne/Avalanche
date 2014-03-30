f1d9d86864e6964e9da719b93bb87483859bed18
Joshua DeGagne <joshdegagne@gmail.com>
2014-03-30 08:24:46 -0400

    Lined up stuff in the controls menu

--- Source/Engine/Engine/ControlsMenuView.cpp
+++ Source/Engine/Engine/ControlsMenuView.cpp
@@ -30,19 +30,27 @@ bool ControlsMenuView::Draw(ControlsMenu* menu)
 
 		spriteBatch->Begin();
 
-		float xPos = 190.0f;
+		float xPos = 170.0f;
 
 		//font->DrawString(sprites, L"AVALANCHE", XMFLOAT2(278, 85), Colors::Purple);
 		spriteFontBig->DrawString(spriteBatch, L"AVALANCHE", XMFLOAT2(MENU_TITLE_X, MENU_TITLE_Y), Colors::Purple);
-		spriteFontNormal->DrawString(spriteBatch, L"HOW TO PLAY", XMFLOAT2(260.0f, 150.0f), Colors::Purple);
+		spriteFontNormal->DrawString(spriteBatch, L"HOW TO PLAY", XMFLOAT2(275.0f, 150.0f), Colors::Purple);
 		
-		spriteFontNormal->DrawString(spriteBatch, L"1.  dodge obstacles", XMFLOAT2(xPos, 213.0f), Colors::MediumPurple);
-		spriteFontNormal->DrawString(spriteBatch, L"2. tackle friends", XMFLOAT2(xPos, 263.0f), Colors::MediumPurple);
-		spriteFontNormal->DrawString(spriteBatch, L"3. don't fall behind", XMFLOAT2(xPos, 313.0f), Colors::MediumPurple);
+		spriteFontNormal->DrawString(spriteBatch, L"1.", XMFLOAT2(xPos, 213.0f), Colors::MediumPurple);
+		spriteFontNormal->DrawString(spriteBatch, L"2.", XMFLOAT2(xPos, 263.0f), Colors::MediumPurple);
+		spriteFontNormal->DrawString(spriteBatch, L"3.", XMFLOAT2(xPos, 313.0f), Colors::MediumPurple);
 
-		spriteFontNormal->DrawString(spriteBatch, L"move:  left analog stick", XMFLOAT2(xPos, 380.0f), Colors::DarkGreen);
-		spriteFontNormal->DrawString(spriteBatch, L"roll:     left or right trigger", XMFLOAT2(xPos, 430.0f), Colors::DarkGreen);
-		spriteFontNormal->DrawString(spriteBatch, L"jump:   A button", XMFLOAT2(xPos, 480.0f), Colors::DarkGreen);
+		spriteFontNormal->DrawString(spriteBatch, L"dodge obstacles", XMFLOAT2(xPos + 55.0f, 213.0f), Colors::MediumPurple);
+		spriteFontNormal->DrawString(spriteBatch, L"tackle friends", XMFLOAT2(xPos + 55.0f, 263.0f), Colors::MediumPurple);
+		spriteFontNormal->DrawString(spriteBatch, L"don't fall behind", XMFLOAT2(xPos + 55.0f, 313.0f), Colors::MediumPurple);
+
+		spriteFontNormal->DrawString(spriteBatch, L"move:", XMFLOAT2(xPos, 380.0f), Colors::DarkGreen);
+		spriteFontNormal->DrawString(spriteBatch, L"roll:", XMFLOAT2(xPos, 430.0f), Colors::DarkGreen);
+		spriteFontNormal->DrawString(spriteBatch, L"jump:", XMFLOAT2(xPos, 480.0f), Colors::DarkGreen);
+
+		spriteFontNormal->DrawString(spriteBatch, L"left analog stick", XMFLOAT2(xPos + 145.0f, 380.0f), Colors::DarkGreen);
+		spriteFontNormal->DrawString(spriteBatch, L"left or right trigger", XMFLOAT2(xPos + 145.0f, 430.0f), Colors::DarkGreen);
+		spriteFontNormal->DrawString(spriteBatch, L"A button", XMFLOAT2(xPos + 145.0f, 480.0f), Colors::DarkGreen);
 
 		/*
 		spriteFontNormal->DrawString(spriteBatch, L"left analog to move", XMFLOAT2(195, 380), Colors::DarkGreen);
