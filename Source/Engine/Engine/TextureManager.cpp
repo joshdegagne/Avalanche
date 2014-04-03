3a5b955259153ed55606c8d596af5b57478c575b
Richard Huynh <richardbaohuynh@gmail.com>
2014-04-03 10:08:18 -0400

    Comments added to TextureManager

--- Source/Engine/Engine/TextureManager.cpp
+++ Source/Engine/Engine/TextureManager.cpp
@@ -1,3 +1,8 @@
+// TextureManager.cpp
+// Loads in textures for ViewModel usage from image files for the Avalanche game project
+// Given a sprite width, we can automatically create an array of sprites in a SpriteTexture 
+// Uses the DirectXTex library for loading image files and copying rectangles
+
 #include "TextureManager.h"
 #include "ITexture.h"
 #include "Texture.h"
@@ -12,12 +17,14 @@ TextureManager::TextureManager()
 	device = nullptr;
 }
 
+// Assigns the manager's ID3D11Device
 bool TextureManager::initialize(Game& game)
 {
 	device = game.getDevice();
 	return true;
 }
 
+// Given an elapsed time, have each loaded SpriteTexture* update its frame index
 void TextureManager::update(float elapsedTime)
 {
 	for(std::vector<SpriteTexture*>::size_type i = spriteTextures.size() - 1; i != (std::vector<int>::size_type) -1; i--) 
@@ -26,17 +33,21 @@ void TextureManager::update(float elapsedTime)
 	}
 }
 
+// Private function to load in an image file for loadTexture or loadSpriteTexture
+// Can be DDS, TGA, PNG, JPG
 ScratchImage* TextureManager::loadTextureFile(WCHAR* filename)
 {
 	ScratchImage* image = new ScratchImage;
 
 	HRESULT result;
 	
+	// Get the filename extension
 	WCHAR ext[_MAX_EXT];
 	_wsplitpath_s( filename, nullptr, 0, nullptr, 0, nullptr, 0, ext, _MAX_EXT );
 
 	if ( _wcsicmp( ext, L".dds" ) == 0 )
 	{
+		// DirectDraw Surface, Microsoft's image container file format
 		result = LoadFromDDSFile( filename, DDS_FLAGS_NONE, NULL, *image );
 	}
 	else if ( _wcsicmp( ext, L".tga" ) == 0 )
@@ -56,53 +67,38 @@ ScratchImage* TextureManager::loadTextureFile(WCHAR* filename)
 	return image;
 }
 
+// Loads an image for use as a static Texture
 Texture* TextureManager::loadTexture(WCHAR* filename)
 {
-	Texture* texture = new Texture;
 
 	ScratchImage* image = loadTextureFile(filename);
 
 	HRESULT result;
-	
-	WCHAR ext[_MAX_EXT];
-	_wsplitpath_s( filename, nullptr, 0, nullptr, 0, nullptr, 0, ext, _MAX_EXT );
 
-	if ( _wcsicmp( ext, L".dds" ) == 0 )
-	{
-		result = LoadFromDDSFile( filename, DDS_FLAGS_NONE, NULL, *image );
-	}
-	else if ( _wcsicmp( ext, L".tga" ) == 0 )
-	{
-		result = LoadFromTGAFile( filename, NULL, *image );
-	}
-	else
-	{
-		result = LoadFromWICFile( filename, WIC_FLAGS_NONE, NULL, *image );
-	}
-	
-	if (FAILED(result)) 
-	{
-		return NULL;
-	}
-
+	// ID3D11ShaderResourceView as the "image" to render
 	ID3D11ShaderResourceView*  pSRV = nullptr;
-    result = CreateShaderResourceView( device,
-        image->GetImages(), image->GetImageCount(),
-        image->GetMetadata(), &pSRV );
+
+	// Create an ID3D11ShaderResourceView* out of the ScratchImage* loaded in from the external file
+    result = CreateShaderResourceView(device, image->GetImages(), image->GetImageCount(), image->GetMetadata(), &pSRV );
 
 	if (FAILED(result)) 
 	{
 		return NULL;
 	}
 	
+	// Make the Texture* and set its view to this newly created one
+	Texture* texture = new Texture;
 	texture->Initialize(pSRV);
 
 	return texture;
 }
 
+// Load a SpriteTexture from a spritesheet image file with a given width to separate each frame
+// First prepares the spritesheet for cropping, then uses CopyRectangle to crop into a vector for the SpriteTexture*
 SpriteTexture* TextureManager::loadSpriteTexture(WCHAR* filename, float spriteWidth) 
 {
 	HRESULT result;
+
 	ScratchImage* DxImage = loadTextureFile(filename);
 
 	const Image* Image_1 = DxImage->GetImages();
@@ -121,14 +117,15 @@ SpriteTexture* TextureManager::loadSpriteTexture(WCHAR* filename, float spriteWi
 		return NULL;
 	}
 
+	// Get the number of frames via the file width and the sprite width
 	int numFrames = 1;
-
 	for (numFrames; spriteWidth < spriteSheetWidth/numFrames; ++numFrames) 
 	{
 	}
 	
 	std::vector<ID3D11ShaderResourceView*> frames(numFrames);
 
+	// Crop out each sprite to make its own ID3D11ShaderResourceView for the frames vector
 	for (int i = 1; i <= numFrames; i++) 
 	{
 		const Image* Image_2 = CropImage.GetImages();
@@ -145,7 +142,7 @@ SpriteTexture* TextureManager::loadSpriteTexture(WCHAR* filename, float spriteWi
 
 		ID3D11ShaderResourceView*  pSRV = nullptr;
 
-		result = CreateShaderResourceView( device, Image_2, CropImage.GetImageCount(), CropImage.GetMetadata(), &pSRV ); // using correct data???
+		result = CreateShaderResourceView( device, Image_2, CropImage.GetImageCount(), CropImage.GetMetadata(), &pSRV );
 		
 		if (FAILED(result)) 
 		{
@@ -157,14 +154,14 @@ SpriteTexture* TextureManager::loadSpriteTexture(WCHAR* filename, float spriteWi
 
 	frames.shrink_to_fit();
 	
-	SpriteTexture* spriteTex = new SpriteTexture(frames, 0.0f, /*1,*/ numFrames);
+	SpriteTexture* spriteTex = new SpriteTexture(frames, 0.0f, numFrames);
 
 	spriteTextures.push_back(spriteTex);
 
 	return spriteTex;
 }
 
-// Deletes pointer to ITexture
+// Deletes the pointer to a given texture
 void TextureManager::unloadTexture(ITexture* texture)
 {
 	if (texture)
--- Source/Engine/Engine/TextureManager.h
+++ Source/Engine/Engine/TextureManager.h
@@ -1,3 +1,5 @@
+// TextureManager.h
+
 #pragma once
 
 #include "ArrayList.h"
@@ -9,8 +11,6 @@
 #include "Game.h"
 #include <vector>
 
-//#define  SPRITE_UPDATE_RATE 1.0f
-
 class Game;
 
 using namespace DirectX;
@@ -20,17 +20,15 @@ class TextureManager : public IManager
 public:
 	TextureManager();
 
-	bool initialize(Game&); 
-	void update(float);  // For SpriteTextures
+	bool initialize(Game&); // Inherited from IManager
+	void update(float);  // Inherited from IManager; here for SpriteTextures
 
 	Texture* loadTexture(WCHAR*); 
-	SpriteTexture* loadSpriteTexture(WCHAR*, float); 
+	SpriteTexture* loadSpriteTexture(WCHAR*, float); // Takes in width of spritesheet cell
 	void unloadTexture(ITexture*); // Makes deletion
 
 private:
 	ID3D11Device* device;
-	std::vector<SpriteTexture*> spriteTextures;
-
-	ScratchImage* loadTextureFile(WCHAR*);
-
+	std::vector<SpriteTexture*> spriteTextures; // For updating each loaded SpriteTexture* (they'll update their own indices)
+	ScratchImage* loadTextureFile(WCHAR*); // Loads a ScratchImage* from a DDS, PNG, other other WIC file
 };
